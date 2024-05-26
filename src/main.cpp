49b246701e8d68129485abfa26d0f1f444b1057f
colesdatwit <colesd@wit.edu>
2024-05-26 12:14:51 -0400

    Project Completed?

--- src/main.cpp
+++ src/main.cpp
@@ -11,28 +11,22 @@
 #include <sstream>
 #include <vector>
 
-//original dimensions: 1280 x 800
-//const int windowWidth = 1000;
-//const int windowHeight = 500;
-
 
 //Shape Parent Class
 class Shape {
 
     public:
-        float objSpeedX;
-        float objSpeedY;
+        float objScale;
+        float objVelocity[2]; //holds speed X and speed Y
         bool drawObj;
         bool drawObjText;
+        bool simulateObj;
+        bool active;
         float objX;
         float objY;
         float objColor[3]; //color is from 0-1
         std::string objName;
         std::string objNewText;
-
-        //object direction booleans
-        bool objRight = true;
-        bool objDown = true;
 
         //virtual destructor
         virtual ~Shape() {};
@@ -54,10 +48,13 @@ class Circle: public Shape {
         Circle(float objRadius, float objSpeedX, float objSpeedY, float objX, float objY, float objR, float objG, float objB, std::string objName) {
         
             this->objRadius = objRadius;
-            this->objSpeedX = objSpeedX;
-            this->objSpeedY = objSpeedY;
+            this->objScale = 1;
+            this->objVelocity[0] = objSpeedX;
+            this->objVelocity[1] = objSpeedY;
             this->drawObj = true;
             this->drawObjText = true;
+            this->simulateObj = true;
+            this->active = true;
             this->objX = objX;
             this->objY = objY;
             this->objColor[0] = objR;
@@ -65,54 +62,19 @@ class Circle: public Shape {
             this->objColor[2] = objB;
             this->objName = objName;
             this->objNewText = objName;
-
-            if (objSpeedX >= 0) {
-                objRight = true;
-            }
-            else {
-                objRight = false;
-                this->objSpeedX = -objSpeedX;
-            }
-
-            if (objSpeedY >= 0) {
-                objDown = true;
-            }
-            else {
-                objDown = false;
-                this->objSpeedY = -objSpeedY;
-            }
         }
 
         void move(int windowWidth, int windowHeight) {
-            if (objRight)
-            {
-                objX += objSpeedX;
-                if (objX + objRadius >= windowWidth)
-                    objRight = false;
-            }
-            else
-            {
-                objX -= objSpeedX;
-                if (objX - objRadius <= 0)
-                    objRight = true;
-            }
-
-            if (objDown)
-            {
-                objY += objSpeedY;
-                if (objY + objRadius >= windowHeight)
-                    objDown = false;
-            }
-            else
-            {
-                objY -= objSpeedY;
-                if (objY - objRadius <= 0)
-                    objDown = true;
-            }
+            objX += objVelocity[0];
+            objY += objVelocity[1];
+            if (objVelocity[0] > 0 && objX + objRadius*objScale >= windowWidth || objVelocity[0] < 0 && objX - objRadius*objScale <= 0)
+                objVelocity[0] = 0 - objVelocity[0];
+            if (objVelocity[1] > 0 && objY + objRadius*objScale >= windowHeight || objVelocity[1] < 0 && objY - objRadius*objScale <= 0)
+                objVelocity[1] = 0 - objVelocity[1];
         }
 
         void draw(Font font) {
-            DrawCircle((int)objX, (int)objY, objRadius, ColorFromNormalized({ objColor[0],objColor[1],objColor[2],1.0f }));
+            DrawCircle((int)objX, (int)objY, objRadius*objScale, ColorFromNormalized({ objColor[0],objColor[1],objColor[2],1.0f }));
             if (drawObjText)
             {
                 //get the size (x and y) of the text object
@@ -126,33 +88,20 @@ class Circle: public Shape {
         }
 
         void addImgui() {
-            //checkboxes, they directly modify the value (which is why we send a reference)
-            ImGui::Checkbox("Draw Object1", &drawObj);
-            ImGui::SameLine();
-            ImGui::Checkbox("Draw Object1 Text", &drawObjText);
+            ImGui::Checkbox("Active", &active);
 
-            //slider, again directly modifies the value and limites between 0 and 300 for this example
-            ImGui::SliderFloat("Radius", &objRadius, 0.0f, 300.0f);
+            //Slider for scale, scale is multiplied with default object bounds to increase draw size and collision detection
+            ImGui::SliderFloat("Scale", &objScale, 0.1f, 5.0f);
 
-            ImGui::SliderFloat("SpeedX", &objSpeedX, 0.0f, 150.0f);
-            ImGui::SliderFloat("SpeedY", &objSpeedY, 0.0f, 75.0f);
+            //Creates two drag boxes on the same line. The one on the left is for X speed and the one on the right is for Y speed. Also creates a title "Velocity" to the right of them
+            ImGui::DragFloat2("Velocity", objVelocity, -1.0f, 1.0f);
 
             //color picker button, directly modifies the color (3 element float array)
-            ImGui::ColorEdit3("Object1 Color", objColor);
+            ImGui::ColorEdit3("Color", objColor);
 
             //text input field, directly modifies the string
-            ImGui::InputText("Text", &objNewText);
-
-            //Buttons
-            if (ImGui::Button("Reset Object1")) {
-                objX = 50.0;
-                objY = 50.0;
-                objRadius = 50;
-            }
-            ImGui::SameLine();
-            if (ImGui::Button("Set Text")) {
-                objName = objNewText;
-            }
+            ImGui::InputText("Name", &objNewText);
+            objName = objNewText;
         }
 };
 
@@ -167,10 +116,13 @@ class Rect : public Shape {
         Rect(float objLength, float objWidth, float objSpeedX, float objSpeedY, float objX, float objY, float objR, float objG, float objB, std::string objName) {
             this->objLength = objLength;
             this->objWidth = objWidth;
-            this->objSpeedX = objSpeedX;
-            this->objSpeedY = objSpeedY;
+            this->objScale = 1;
+            this->objVelocity[0] = objSpeedX;
+            this->objVelocity[1] = objSpeedY;
             this->drawObj = true;
             this->drawObjText = true;
+            this->simulateObj = true;
+            this->active = true;
             this->objX = objX;
             this->objY = objY;
             this->objColor[0] = objR;
@@ -178,55 +130,20 @@ class Rect : public Shape {
             this->objColor[2] = objB;
             this->objName = objName;
             this->objNewText = objName;
-
-            if (objSpeedX >= 0) {
-                objRight = true;
-            }
-            else {
-                objRight = false;
-                this->objSpeedX = -objSpeedX;
-            }
-
-            if (objSpeedY >= 0) {
-                objDown = true;
-            }
-            else {
-                objDown = false;
-                this->objSpeedY = -objSpeedY;
-            }
         }
 
         void move(int windowWidth, int windowHeight) {
-            if (objRight)
-            {
-                objX += objSpeedX;
-                if (objX + objWidth >= windowWidth)
-                    objRight = false;
-            }
-            else
-            {
-                objX -= objSpeedX;
-                if (objX <= 0)
-                    objRight = true;
-            }
-
-            if (objDown)
-            {
-                objY += objSpeedY;
-                if (objY + objLength >= windowHeight)
-                    objDown = false;
-            }
-            else
-            {
-                objY -= objSpeedY;
-                if (objY <= 0)
-                    objDown = true;
-            }
+            objX += objVelocity[0];
+            objY += objVelocity[1];
+            if (objVelocity[0] > 0 && objX + objWidth*objScale >= windowWidth || objVelocity[0] < 0 && objX <= 0)
+                objVelocity[0] = 0 - objVelocity[0];
+            if (objVelocity[1] > 0 && objY + objLength*objScale >= windowHeight || objVelocity[1] < 0 && objY <= 0)
+                objVelocity[1] = 0 - objVelocity[1];
         }
 
         void draw(Font font) {
             if (drawObj) {
-                DrawRectangle((int)objX, (int)objY, objWidth, objLength, ColorFromNormalized({ objColor[0],objColor[1],objColor[2],1.0f }));
+                DrawRectangle((int)objX, (int)objY, objWidth*objScale, objLength*objScale, ColorFromNormalized({ objColor[0],objColor[1],objColor[2],1.0f }));
 
                 if (drawObjText)
                 {
@@ -236,49 +153,40 @@ class Rect : public Shape {
 
                     //draw the text (using the text size to help draw it in the corner
                     //(font,c string, vector2, font size, font spaceing, color)
-                    DrawTextEx(font, objName.c_str(), { (objX + objWidth/2) - (textSize.x / 2), (objY + objLength/2) - (textSize.y / 2) }, 18, 1, WHITE);
+                    DrawTextEx(font, objName.c_str(), { (objX + objWidth*objScale/2) - (textSize.x / 2), (objY + objLength*objScale/2) - (textSize.y / 2) }, 18, 1, WHITE);
                 }
             }
         }
 
         void addImgui() {
-            ImGui::Checkbox("Draw Object2", &drawObj);
-            ImGui::SameLine();
-            ImGui::Checkbox("Draw Object2 Text", &drawObjText);
+            //Checkbox that modifies the active boolean, when false, the object is not drawn and does not move
+            ImGui::Checkbox("Active", &active);
 
-            //slider, again directly modifies the value and limites between 0 and 300 for this example
-            ImGui::SliderFloat("Length", &objLength, 0.0f, 300.0f);
-            ImGui::SliderFloat("Width", &objWidth, 0.0f, 300.0f);
+            //Slider for scale, scale is multiplied with default object bounds to increase draw size and collision detection
+            ImGui::SliderFloat("Scale", &objScale, 0.1f, 5.0f);
 
-            ImGui::SliderFloat("SpeedX", &objSpeedX, 0.0f, 150.0f);
-            ImGui::SliderFloat("SpeedY", &objSpeedY, 0.0f, 75.0f);
+            //Creates two drag boxes on the same line with the title Velocity to the right of them
+            ImGui::DragFloat2("Velocity", objVelocity, -1.0f, 1.0f);
 
             //color picker button, directly modifies the color (3 element float array)
-            ImGui::ColorEdit3("Object1 Color", objColor);
+            ImGui::ColorEdit3("Color", objColor);
 
             //text input field, directly modifies the string
-            ImGui::InputText("Text", &objNewText);
-
-            //Buttons
-            if (ImGui::Button("Reset Object1")) {
-                objX = 1000.0;
-                objY = 50.0;
-                objLength = 30;
-                objWidth = 60;
-            }
-            ImGui::SameLine();
-            if (ImGui::Button("Set Text")) {
-                objName = objNewText;
-            }
+            ImGui::InputText("Name", &objNewText);
+            objName = objNewText;
         }
 };
 
 //object update function
 void updateShape(Shape& obj, Font font, int windowWidth, int windowHeight) {
-    if (obj.drawObj) {
-        obj.draw(font);
+    if (obj.active) {
+        if (obj.drawObj) {
+            obj.draw(font);
+        }
+        if (obj.simulateObj) {
+            obj.move(windowWidth, windowHeight);
+        }
     }
-    obj.move(windowWidth, windowHeight);
 }
 
 
@@ -302,7 +210,7 @@ int main(void)
     std::string line;
     
 
-        
+        //reading from file
         while(getline(input, line)) {
 
             std::istringstream iss(line);
@@ -350,6 +258,7 @@ int main(void)
                 float tGreen = std::stof(tempv[7]);
                 float tBlue = std::stof(tempv[8]);
                 
+                //Object Parameters: Circle(float Radius, float SpeedX, float SpeedY, float X, float Y, float Red, float Green, float Blue, string Name)   
                 circles.push_back(Circle(tRadius, tSpeedX, tSpeedY, tX, tY, tRed, tGreen, tBlue, tName));
                 
             }
@@ -375,6 +284,7 @@ int main(void)
                 float tGreen = std::stof(tempv[7]);
                 float tBlue = std::stof(tempv[8]);
 
+                //Object Parameters: Rect(float Length, float Width, float SpeedX, float SpeedY, float X, float Y, float Red, float Green, float Blue, string Name)
                 rects.push_back(Rect(tLength, tWidth, tSpeedX, tSpeedY, tX, tY, tRed, tGreen, tBlue, tName));
             }
         }
@@ -382,7 +292,7 @@ int main(void)
         input.close();
 
     SetConfigFlags(FLAG_WINDOW_HIGHDPI);
-    InitWindow(screenWidth, screenHeight, "Assignment1Soln");
+    InitWindow(screenWidth, screenHeight, "Assignment1 BNDC");
 
     //initialize the raylib ImGui backend
     rlImGuiSetup(true);
@@ -394,19 +304,12 @@ int main(void)
 
     // General variables
     //--------------------------------------------------------------------------------------
-
-
-    //circle & rect initialization
-
-    //Object Parameters: Circle(float Radius, float SpeedX, float SpeedY, float X, float Y, float Red, float Green, float Blue, string Name)
-    // Circle obj1 = Circle(50.0f, 1.0f, 0.5f, 50.0f, 50.0f, 0.0f, 0.0f, 1.0f, "Object1");
-
-    //Object Parameters: Rect(float Length, float Width, float SpeedX, float SpeedY, float X, float Y, float Red, float Green, float Blue, string Name)
-    //Rect obj2 = Rect(30, 60, 1.0f, 0.5f, 900.0f, 50.0f, 1.0f, 0.5f, 0.0f, "Object2");
-
-
     //vector for shape storage
     //std::vector<Shape*> shapes;
+
+    bool drawShapes = true;
+    bool drawShapeText = true;
+    bool simulate = true;
 
     bool posY = true;
     bool posX = true;
@@ -419,8 +322,7 @@ int main(void)
     //load a font (Raylib gives warning if font can't be found, then uses default as fallback)
     Font font = LoadFont("assets/Orbitron.ttf");
 
-    //shapes[0]->draw(font);
-    //shapes[1]->draw(font);
+    
 
     // Main game loop
     //--------------------------------------------------------------------------------------
@@ -444,16 +346,20 @@ int main(void)
 
         //iterator for drawing shapes from vector; currently not working
 
-        
-        for (int i = 0; i < rects.size(); i++) {            
+        for (int i = 0; i < rects.size(); i++) {
+            rects[i].drawObj = drawShapes;
+            rects[i].drawObjText = drawShapeText;
+            rects[i].simulateObj = simulate;
             updateShape(rects[i], font, screenWidth, screenHeight);
         }
 
         for (int i = 0; i < circles.size(); i++) {
+            circles[i].drawObj = drawShapes;
+            circles[i].drawObjText = drawShapeText;
+            circles[i].simulateObj = simulate;
             updateShape(circles[i], font, screenWidth, screenHeight);
         }
         
-
 
         //updateShape(obj1, font);
         //updateShape(obj2, font);
@@ -476,17 +382,23 @@ int main(void)
 
         //sets the next window to be at this position
         //also uses the imgui.ini that gets created at first run
-        ImGui::SetNextWindowSize(ImVec2(350, 250));
+        ImGui::SetNextWindowSize(ImVec2(350, 350));
         //creates a new window
         ImGui::Begin("Assignment 1 Controls", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
-        ImGui::Text("All Shape Controls");
+        ImGui::SeparatorText("All Shape Controls");
 
-        ImGui::Text("Selected Shape Controls");
+        ImGui::Checkbox("Draw Shapes", &drawShapes);
+        ImGui::SameLine();
+        ImGui::Checkbox("Draw Text", &drawShapeText);
+        ImGui::SameLine();
+        ImGui::Checkbox("Simulate", &simulate);
+
+        ImGui::SeparatorText("Selected Shape Controls");
         //A listbox to select which object is being modified
 
         const char* items[] = {"CGreen","CCyan","CYellow","RRed","RMagenta","RBlue"};
         static int item_current = 0;
-        ImGui::Combo("Object\nSelect", &item_current, items, IM_ARRAYSIZE(items), 1);
+        ImGui::Combo("Shape", &item_current, items, IM_ARRAYSIZE(items), 0);
 
         for (int i = 0; i < circles.size(); i++) {
             if (item_current == i) //when Text is selected in the Object Select ComboBox
